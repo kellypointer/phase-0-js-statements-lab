fc6223ed0510ec41c1522557b32f771e1aff156a
Kelly Pointer <kellyjoanpointer@gmail.com>
2025-02-15 17:51:02 -0600

    Statements Lab

--- gardenManagement.js
+++ gardenManagement.js
@@ -1,3 +1,28 @@
 const temperature = 90;
 const timeOfDay = "morning";
 let soilMoisture = 30;
+if (temperature > 80) {
+    console.log("Watering on");
+    
+} else {
+    console.log("Watering off");
+
+}
+let lightstatus;
+if (timeOfDay === "evening") {
+    lightstatus = "Lights on";
+
+} else if (timeOfDay === "night") {
+    lightstatus = "Lights on"; 
+
+} else {
+    lightstatus = "Lights off";
+
+}
+    console.log(lightstatus)
+    
+while (soilMoisture <= 40) {
+        console.log(soilMoisture);
+        soilMoisture += 5
+      }
+
